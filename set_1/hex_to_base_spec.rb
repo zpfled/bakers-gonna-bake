7bb38843b636677d18af0b93a5fd5231a6372c53
Zach Pflederer <zach@giveforward.com>
2015-01-24 15:08:40 -0600

    set 1; challenges 1 & 2

---
+++ set_1/hex_to_base_spec.rb
@@ -0,0 +1,24 @@
+require './hex_to_base'
+
+
+describe Converters do
+
+  describe 'set 1' do
+    describe 'challenge 1' do
+      it 'completed' do
+        hex_string = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
+        base64_string = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
+        expect(Converters.hex_to_base64(hex_string)).to eq(base64_string)
+      end
+    end
+
+    describe 'challenge 2' do
+      it 'completed' do
+        hex_string = '1c0111001f010100061a024b53535009181c'
+        key = '686974207468652062756c6c277320657965'
+        result = '746865206b696420646f6e277420706c6179'
+        expect(Converters.hex_fixed_xor(hex_string, key)).to eq(result)
+      end
+    end
+  end
+end
