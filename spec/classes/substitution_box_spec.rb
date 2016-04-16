91c16128c953eff237e304bfca477a7d3682c548
Zach Pflederer <zach@giveforward.com>
2016-04-16 10:49:05 -0500

    working sbox, but only the copied version

--- spec/classes/substitution_box_spec.rb
+++ spec/classes/substitution_box_spec.rb
@@ -3,7 +3,13 @@ require_relative '../../classes/substitution_box'
 describe SubstitutionBox do
   describe '#transform' do
     it 'transforms 0x9a to 0xb8' do
+<<<<<<< HEAD
       expect(SubstitutionBox.new.transform(0x9a)).to eq 0Xb8
+=======
+      # expect(SubstitutionBox.new.transform(0x9a)).to eq 0Xb8
+      # expect(SubstitutionBox.new.transform(0xdb)).to eq 0x9f
+      expect(SubstitutionBox.new.sbox(0x9a)).to eq 0Xb8
+>>>>>>> working sbox, but only the copied version
     end
   end
 end
